15c69c95616396422f88a50da17c6989b30e7465
Pieter <pieter@sition.nl>
2016-08-10 17:15:12 +0200

    Changed line endings to unix style

--- Controllers/Frontend/SitionSooqr.php
+++ Controllers/Frontend/SitionSooqr.php
@@ -7,39 +7,10 @@ class Shopware_Controllers_Frontend_SitionSooqr extends Enlight_Controller_Actio
 {
 	public function xmlAction() 
 	{	
-		//Shopware()->Debuglogger()->info('some message from sition sooqr');
-		
 		$sooqr = new SooqrXml;
 
 		$sooqr->outputXml();
 		
-// 		$iter = new RecursiveIteratorIterator(
-// 		    new RecursiveDirectoryIterator(__DIR__ . "/../../../../../../../../media/image", RecursiveDirectoryIterator::SKIP_DOTS),
-// 		    RecursiveIteratorIterator::SELF_FIRST,
-// 		    RecursiveIteratorIterator::CATCH_GET_CHILD // Ignore "Permission denied"
-// 		);
-
-// 		echo "iter";
-// $paths = array($root);
-// foreach ($iter as $path => $dir) {
-//     if ($dir->isDir()) {
-//         $paths[] = $path;
-//     }
-// }
-
-// print_r($paths);
-
-		// pr(inst(Shopware()->Config()));preg_quote("Ort-ok17p1-1")
-		// Shopware()->Config()->bla();
-
-		// echo Shopware()->Config()->get("host");
-
-		// pr(Shopware()->Modules()->Articles()->sGetComparisons());
-
-		// $sitemap = Shopware()->Models()->getRepository('sitemapxml.repository');
-
-		// pr(inst($sitemap));
-
 		// exit request, don't render a view
 		exit();
 	}
